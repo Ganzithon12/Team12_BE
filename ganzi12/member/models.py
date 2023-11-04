3d5c2813c5de0e242fa2059efd791427e6e7727c
ShinSub98 <wjdtjq26@naver.com>
2023-11-04 21:06:37 +0900

    fix: set related_name of groups and user_permissions of CustomUser model

--- ganzi12/member/models.py
+++ ganzi12/member/models.py
@@ -1,15 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 # Create your models here.
 def user_photo_path(instance, filename):
     return f'profile_image/{instance.member.pk}/{filename}'
 
 class CustomUser(AbstractUser):
-  REQUIRED_FIELDS = []
-  email = None
-  nickname = models.CharField(max_length=100)
-  profile_image = models.ImageField(upload_to=user_photo_path, null = True)
+    REQUIRED_FIELDS = []
+    email = None
+    nickname = models.CharField(max_length=100)
+    profile_image = models.ImageField(upload_to=user_photo_path, null = True)
 
-  def __str__(self):
-    return self.nickname
+    groups = models.ManyToManyField(Group, related_name='custom_users_groups')
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_users_permissions')
+
+    def __str__(self):
+        return self.nickname
