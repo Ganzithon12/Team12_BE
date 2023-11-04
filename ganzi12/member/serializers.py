bd75ac1679fdc17dc38a19587d3f82e9e5ae5fad
ShinSub98 <wjdtjq26@naver.com>
2023-11-04 22:09:41 +0900

    feat: add UserInfoSerializer

--- ganzi12/member/serializers.py
+++ ganzi12/member/serializers.py
@@ -16,3 +16,9 @@ class SignupSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=150)
     password = serializers.CharField(max_length=128)
+
+
+class UserInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'nickname', 'profile_image']
