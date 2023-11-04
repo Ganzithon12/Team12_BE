c07c7a2e3372c8276d3d6233df54e7d89b96839d
ShinSub98 <wjdtjq26@naver.com>
2023-11-04 21:39:45 +0900

    feat: add urls

--- ganzi12/member/urls.py
+++ ganzi12/member/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from .views import *
 
 app_name = 'member'
 
 urlpatterns = [
-    
+    path('signup/', signup),
+    path('login/', login),
+    path('logout/', logout),
 ]
