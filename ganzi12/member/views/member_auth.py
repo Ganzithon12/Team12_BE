c2217c4bdf4ca6df6273d2b47e332430f97d9c0c
ShinSub98 <wjdtjq26@naver.com>
2023-11-04 21:30:04 +0900

    feat: add login view

--- ganzi12/member/views/member_auth.py
+++ ganzi12/member/views/member_auth.py
@@ -2,7 +2,7 @@ from ..models import CustomUser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from ..serializers import LoginSerializer, SignupSerializer
+from ..serializers import *
 from django.contrib import auth
 from django.contrib.auth.hashers import make_password
 
@@ -12,11 +12,40 @@ def signup(request):
     if serializer.is_valid():
         new_user = serializer.save(password = make_password(serializer.validated_data['password']))
         auth.login(request, new_user)
+        user_data = CustomUserDetailSerializer(new_user)
         res = {
-            "msg" : "회원가입 성공"
+            "msg" : "회원가입 성공",
+            "data" : user_data.data
         }
         return Response(res)
     res = {
         "msg" : "회원가입 실패"
+    }
+    return Response(res)
+
+
+@api_view(['POST'])
+def login(request):
+    serializer = LoginSerializer(data=request.data)
+    if serializer.is_valid():
+        user = auth.authenticate(
+            request=request,
+            username=serializer.data['username'],
+            password=serializer.data['password']
+        )
+        if user is not None:
+            auth.login(request, user)
+            user_data = CustomUserDetailSerializer(user)
+            res = {
+                "msg" : "로그인 성공",
+                "data" : user_data.data
+            }
+            return Response(res)
+        res = {
+            "msg" : "존재하지 않는 회원"
+        }
+        return Response(res)
+    res = {
+        "msg" : "잘못된 요청"
     }
     return Response(res)
