f30920b4acfb139f544e1ceecb65ea49ca686a75
ShinSub98 <wjdtjq26@naver.com>
2023-11-04 21:00:30 +0900

    feat: add CustomUser model

--- ganzi12/member/models.py
+++ ganzi12/member/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+def user_photo_path(instance, filename):
+    return f'profile_image/{instance.member.pk}/{filename}'
+
+class CustomUser(AbstractUser):
+  REQUIRED_FIELDS = []
+  email = None
+  nickname = models.CharField(max_length=100)
+  profile_image = models.ImageField(upload_to=user_photo_path, null = True)
+
+  def __str__(self):
+    return self.nickname
