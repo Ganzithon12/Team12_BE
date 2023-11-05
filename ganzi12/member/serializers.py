307bce12c0f0be9bde9816cd4c8aceca4641794c
zzimnii <happykeil@naver.com>
2023-11-05 15:07:25 +0900

    fix: signup with email,password,password2

--- ganzi12/member/serializers.py
+++ ganzi12/member/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import *
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token 
+
 
 class CustomUserDetailSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,11 +11,30 @@ class CustomUserDetailSerializer(serializers.ModelSerializer):
 
 
 class SignupSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = CustomUser
-        fields = ['email', 'password', 'nickname', 'profile_image']
+        fields = ['email', 'password', 'password2', 'nickname', 'profile_image']
 
-
+    def validate(self, data):
+        if data['password']!= data['password2']:
+            raise serializers.ValidationError(
+                {"password": "Passwords don't match."})
+        
+        return data
+    
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            email=validated_data['email'],
+            nickname=validated_data['nickname'],
+        )
+        password=validated_data['password']
+        user.set_password(password)
+        user.save()
+        token = Token.objects.create(user=user)
+        return user
+    
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
