777195b7e9d424d2f2c51cec61fa0709f02732c1
ShinSub98 <wjdtjq26@naver.com>
2023-11-06 13:13:19 +0900

    feat: add ChallengeCompleted model

--- ganzi12/challenges/models.py
+++ ganzi12/challenges/models.py
@@ -13,3 +13,12 @@ class Challenge(models.Model):
 
     def __str__(self):
         return self.title
+    
+class ChallengeCompleted(models.Model):
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
+    writer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    challenge_image = models.ImageField(blang = False, upload_to=f"challenges/{challenge.title}")
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.writer} - {self.challenge} ({self.created_at})"
