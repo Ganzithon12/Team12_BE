23707ec112e59c04eec54449ea2144cc2696e0b6
ShinSub98 <wjdtjq26@naver.com>
2023-11-06 14:40:48 +0900

    refactor: add challenge list url

--- ganzi12/challenges/urls.py
+++ ganzi12/challenges/urls.py
@@ -4,6 +4,7 @@ from .views import *
 app_name = 'challenges'
 
 urlpatterns = [
+    path('<str:state>/', ChallengeList.as_view()),
     path('create/', CreateChallenge.as_view()),
     path('certify/<int:challenge_id>/', CreateCertify.as_view()),
     path('participate/<int:challenge_id>/', ParticipateChallenge.as_view()),
--- ganzi12/challenges/views/__init__.py
+++ ganzi12/challenges/views/__init__.py
@@ -1,3 +1,4 @@
 from .challenge_update_view import *
 from .challenge_certify_view import *
 from .challenge_participate_view import *
+from .challenge_list_view import *
